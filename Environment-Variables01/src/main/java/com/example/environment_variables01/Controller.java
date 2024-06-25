8c0b9bdf32ce6e02e2170904a4a1824b3cf409b1
AleDev <aledev@example.com>
2024-06-25 19:19:56 +0200

    consegna

---
+++ Environment-Variables01/src/main/java/com/example/environment_variables01/Controller.java
@@ -0,0 +1,22 @@
+package com.example.environment_variables01;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("properties")
+public class Controller {
+
+    @Autowired
+    private PropertiesService propertiesService;
+
+    @GetMapping
+    public Collection<String> getProperties() {
+
+        return propertiesService.getProperties();
+    }
+}
